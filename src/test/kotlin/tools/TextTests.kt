850ec296522a23fada6a0acb4e3a70d4c0fc0638
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-01-13 15:38:01 +0100

    Add tests

--- src/test/kotlin/tools/TextTests.kt
+++ src/test/kotlin/tools/TextTests.kt
@@ -1,5 +1,6 @@
 package tools.math.tools
 
+import tools.match
 import tools.toWords
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -7,8 +8,17 @@ import kotlin.test.assertEquals
 
 class TextTests {
     @Test
-    fun testText() {
-        val words = "abc_def! hi-jkl \tMoù1p!345   qRSt\n1Z".toWords().toList()
+    fun testWord() {
+        val words = "abc_def! hi-jkl \tMoù1p!345   qRSt\n1Z".toWords()
         assertEquals(listOf("abc_def!", "hi-jkl", "Moù1p!345", "qRSt", "1Z"), words)
+    }
+
+    @Test
+    fun testMatch() {
+        val matchOK = "ABCD = 123->45".match("(.*) = (\\d+)->(\\d+)".toRegex())
+        assertEquals(listOf("ABCD", "123", "45"), matchOK)
+
+        val matchKO = "ABCD = 123 -> 45".match("(.*) = (\\d+)->(\\d+)".toRegex())
+        assertEquals(null, matchKO)
     }
 }
