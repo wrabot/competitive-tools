5adc40444906a80651a6aba7a34518dbc42c40b3
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-01-15 10:54:45 +0100

    Refactor board

--- src/main/kotlin/tools/board/Board.kt
+++ src/main/kotlin/tools/board/Board.kt
@@ -1,11 +1,12 @@
 package tools.board
 
 class Board<T>(val width: Int, val height: Int, val cells: List<T>) {
-    data class XY(val x: Int, val y: Int) {
+    data class XY(val x: Int, val y: Int) : Comparable<XY> {
         operator fun minus(other: XY) = XY(x - other.x, y - other.y)
         operator fun plus(other: XY) = XY(x + other.x, y + other.y)
         operator fun times(factor: Int) = XY(x * factor, y * factor)
         fun manhattan() = kotlin.math.abs(x) + kotlin.math.abs(y)
+        override fun compareTo(other: XY) = y.compareTo(other.y).let { if (it != 0) it else x.compareTo(other.x) }
     }
 
     val xRange = 0 until width
@@ -19,7 +20,7 @@ class Board<T>(val width: Int, val height: Int, val cells: List<T>) {
     override fun toString() = cells.chunked(width) { it.joinToString("") }.joinToString("\n")
 
     fun toString(start: XY, end: XY) = cells.chunked(width) {
-        it.joinToString("").substring(start.x, end.x + 1)
+        it.subList(start.x, end.x + 1).joinToString("")
     }.subList(start.y, end.y + 1).joinToString("\n")
 
     private fun isValid(x: Int, y: Int) = x in xRange && y in yRange
@@ -31,19 +32,21 @@ class Board<T>(val width: Int, val height: Int, val cells: List<T>) {
     fun getOrNull(xy: XY) = getOrNull(xy.x, xy.y)
     operator fun get(xy: XY) = get(xy.x, xy.y)
 
-    val directions4 = listOf(XY(1, 0), XY(0, -1), XY(-1, 0), XY(0, 1))
-    val directions8 = directions4 + listOf(XY(1, -1), XY(-1, -1), XY(-1, 1), XY(1, 1))
-
-    fun neighbors4(xy: XY) = directions4.map { xy + it }.filter { isValid(it) }
-    fun neighbors8(xy: XY) = directions8.map { xy + it }.filter { isValid(it) }
+    fun neighbors4(xy: XY) = xy4dir.map { xy + it }.filter { isValid(it) }
+    fun neighbors8(xy: XY) = xy8dir.map { xy + it }.filter { isValid(it) }
 
     fun zone4(xy: XY, predicate: (XY) -> Boolean) = zone(xy) { neighbors4(it).filter(predicate) }
 
-    private fun zone(xy: XY, neighbors: (XY) -> List<XY>): Set<XY> =
-        sortedSetOf<XY>({ p1, p2 -> (p2.y - p1.y) * width + p2.x - p1.x }, xy).apply {
-            val todo = mutableListOf(xy)
+    private fun zone(start: XY, neighbors: (XY) -> List<XY>): Set<XY> =
+        sortedSetOf(start).apply {
+            val todo = mutableListOf(start)
             while (true) neighbors(todo.removeFirstOrNull() ?: break).forEach { if (add(it)) todo.add(it) }
         }
+
+    companion object {
+        val xy4dir = listOf(XY(1, 0), XY(0, -1), XY(-1, 0), XY(0, 1))
+        val xy8dir = xy4dir + listOf(XY(1, -1), XY(-1, -1), XY(-1, 1), XY(1, 1))
+    }
 }
 
 fun <T> List<String>.toBoard(cell: (Char) -> T) = Board(get(0).length, size, flatMap { it.map(cell) })
