3fe3fdd4dea5c5d4afa62d398b1cf1ed26f8d4b2
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-01-15 16:41:06 +0100

    Refactor polynomial interpolation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "wrabot.competitive"
-version = 0.8
+version = 0.9
 
 repositories {
     mavenCentral()
--- src/main/kotlin/tools/math/PolynomialInterpolation.kt
+++ src/main/kotlin/tools/math/PolynomialInterpolation.kt
@@ -1,10 +1,11 @@
 package tools.math
 
-class PolynomialInterpolation(vararg values: Double) {
-    private val coefficients = values.toList().coefficients().reversed()
+data class PolynomialInterpolation(val values: List<Double>) {
+    constructor(vararg values: Double) : this(values.toList())
 
     operator fun invoke(x: Double) = coefficients.foldRightIndexed(0.0) { i, c, acc -> acc * (x + i) / (i + 1) + c }
 
+    private val coefficients = values.coefficients().reversed()
     private fun List<Double>.coefficients(): List<Double> =
         if (all { it == 0.0 }) emptyList() else zipWithNext().map { it.second - it.first }.coefficients() + last()
 }
