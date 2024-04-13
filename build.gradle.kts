b37e3d4ef1eba6aa5b46c88791d5d070e6adea67
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-04-13 21:36:29 +0200

    test add sources

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
 }
 
+tasks.jar {    
+    from(sourceSets["main"].allSource)
+}
+
 tasks.test {
     useJUnitPlatform()
 }
