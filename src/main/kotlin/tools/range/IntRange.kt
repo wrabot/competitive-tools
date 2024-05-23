4bd9a2c6f7df60a9f5b27657b8f1c26f6aaad162
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-05-23 12:02:37 +0200

    Update range

--- src/main/kotlin/tools/range/IntRange.kt
+++ src/main/kotlin/tools/range/IntRange.kt
@@ -3,7 +3,6 @@ package tools.range
 import kotlin.math.ceil
 import kotlin.math.floor
 
-fun IntRange.move(offset: Int) = first + offset..last + offset
 fun rangeMinMax(a: Int, b: Int) = a.coerceAtMost(b)..a.coerceAtLeast(b)
 fun intRange(r: ClosedFloatingPointRange<Double>) = ceil(r.start).toInt()..floor(r.endInclusive).toInt()
 operator fun IntRange.contains(d: Double) = d >= start && d <= endInclusive
--- src/main/kotlin/tools/range/Range.kt
+++ src/main/kotlin/tools/range/Range.kt
@@ -1,6 +1,9 @@
 package tools.range
 
-val IntRange.size get() = endInclusive - start + 1
-val LongRange.size get() = endInclusive - start + 1
+val IntRange.size get() = last - start + 1
+val LongRange.size get() = last - start + 1
+fun IntRange.move(offset: Int) = first + offset..last + offset
+fun LongRange.move(offset: Int) = first + offset..last + offset
+
 fun <T : Comparable<T>> ClosedRange<T>.hasIntersection(other: ClosedRange<T>) =
     start in other || endInclusive in other || other.start in this
