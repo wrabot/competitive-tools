ba35e0206df6b167fe45c4207070b09f3fa2fe03
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-11-03 21:28:01 +0100

    Refactor annealling tests

--- src/test/kotlin/tools/optimization/SimulatedAnnealingTests.kt
+++ src/test/kotlin/tools/optimization/SimulatedAnnealingTests.kt
@@ -1,13 +1,14 @@
 package tools.optimization
 
 import tools.Point
+import java.util.*
 import kotlin.math.sqrt
 import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class SimulatedAnnealingTests {
-    val towns = listOf(
+    private val towns = listOf(
         Point(565.0, 575.0),
         Point(25.0, 185.0),
         Point(345.0, 750.0),
@@ -16,20 +17,16 @@ class SimulatedAnnealingTests {
         Point(880.0, 660.0),
     )
 
-    private val distances = towns.indices.associateWith { i ->
-        towns.indices.associateWith { j ->
-            sqrt((towns[i] - towns[j]).norm2())
-        }
-    }
+    private val distances = 
+        List(towns.size) { a -> DoubleArray(towns.size) { b -> sqrt((towns[a] - towns[b]).norm2()) } }
 
-    inner class State(val towns: List<Int>) : SimulatedAnnealing.State<State> {
-        override val energy: Double = (towns + towns.first()).zipWithNext { a, b -> distances[a]!![b]!! }.sum()
+    inner class State(private val path: List<Int>) : SimulatedAnnealing.State<State> {
+        override val energy: Double = 
+            distances[0][path.first()] + path.zipWithNext { a, b -> distances[a][b] }.sum() + distances[path.last()][0]
 
-        override fun move(random: Random): State {
-            val i = random.nextInt(1, towns.size - 1)
-            val j = random.nextInt(i + 1, towns.size)
-            return State(towns.subList(0, i) + towns[j] + towns.subList(i + 1, j) + towns[i] + towns.subList(j + 1, towns.size))
-        }
+        override fun move(random: Random) = State(path.toMutableList().apply {
+            Collections.swap(this, random.nextInt(path.size), random.nextInt(path.size))
+        })
     }
 
     @Test
