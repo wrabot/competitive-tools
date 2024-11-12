fef959d51a0ee9abaa7536f9fa1de237bc74cae6
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-11-12 10:39:31 +0100

    Add mcs

--- src/main/kotlin/tools/graph/Graph.kt
+++ src/main/kotlin/tools/graph/Graph.kt
@@ -1,12 +1,12 @@
 package tools.graph
 
-fun bfs(size: Int, start: Int, block: (Int) -> List<Int>?) {
+fun bfs(size: Int, start: Int, neighbors: (Int) -> List<Int>?) {
     val todo = mutableListOf(start)
     val discovered = BooleanArray(size)
     discovered[start] = true
     while (true) {
         val current = todo.removeFirstOrNull() ?: break
-        for (next in block(current) ?: break) {
+        for (next in neighbors(current) ?: break) {
             if (!discovered[next]) {
                 discovered[next] = true
                 todo.add(next)
@@ -15,13 +15,23 @@ fun bfs(size: Int, start: Int, block: (Int) -> List<Int>?) {
     }
 }
 
-fun dfs(size: Int, start: Int, block: (Int) -> List<Int>?) {
+fun dfs(size: Int, start: Int, neighbors: (Int) -> List<Int>?) {
     val todo = mutableListOf(start)
     val visited = BooleanArray(size)
     while (true) {
         val current = todo.removeLastOrNull() ?: break
         if (visited[current]) continue
         visited[current] = true
-        todo.addAll(block(current) ?: break)
+        todo.addAll(neighbors(current) ?: break)
+    }
+}
+
+fun mcs(size: Int, start: Int, neighbors: (Int) -> List<Int>) {
+    val weights = IntArray(size)
+    weights[start] = 1
+    repeat(size) {
+        val v = weights.indices.maxBy { weights[it] }
+        weights[v] = -1
+        neighbors(v).forEach { if (weights[it] != -1) weights[it]++ }
     }
 }
--- src/test/kotlin/tools/graph/GraphTests.kt
+++ src/test/kotlin/tools/graph/GraphTests.kt
@@ -17,41 +17,69 @@ class GraphTests {
             else -> emptyList()
         }
     }
-    private val depthFullResult = ((1 until size).toList() + 0).reversed()
-    private val depthTreeResult =
-        ((groupSize until size step groupSize).flatMap { group -> ((1..9) + 0).map { it + group } } + 0).reversed()
-    private val breathTreeResult =
-        ((0 until size step groupSize) + (groupSize until size step groupSize).flatMap { group -> (1..9).map { it + group } })
 
     @Test
     fun testBfs() {
         val path = mutableListOf<Int>()
+
         bfs(size, start) {
             path.add(it)
             fullConnected(it)
         }
-        assertEquals((0..99).toList(), path)
+        val breathFullResult = (0..99).toList()
+        assertEquals(breathFullResult, path)
+
         path.clear()
+
         bfs(size, start) {
             path.add(it)
             tree(it)
         }
+        val breathTreeResult = (0 until size step groupSize) +
+                (groupSize until size step groupSize).flatMap { group -> (1..9).map { it + group } }
         assertEquals(breathTreeResult, path)
     }
 
     @Test
     fun testDfs() {
         val path = mutableListOf<Int>()
+
         dfs(size, start) {
             path.add(it)
             fullConnected(it)
         }
+        val depthFullResult = ((1 until size).toList() + 0).reversed()
         assertEquals(depthFullResult, path)
+
         path.clear()
+
         dfs(size, start) {
             path.add(it)
             tree(it)
         }
+        val depthTreeResult = ((groupSize until size step groupSize).flatMap { group -> ((1..9) + 0).map { it + group } } + 0)
+            .reversed()
         assertEquals(depthTreeResult, path)
+    }
+
+    @Test
+    fun testMCS() {
+        val path = mutableListOf<Int>()
+
+        mcs(size, start) {
+            path.add(it)
+            fullConnected(it)
+        }
+        val mcsFullResult = (0..99).toList()
+        assertEquals(mcsFullResult, path)
+
+        path.clear()
+
+        mcs(size, start) {
+            path.add(it)
+            tree(it)
+        }
+        val mcsTreeResult = listOf(0) + (10..99).toList() + (1..9).toList()
+        assertEquals(mcsTreeResult, path)
     }
 }
